10b5357511bc5d089af9338d7d4cd06c6fedb499
byrain <byrain@outlook.com>
2022-07-04 11:55:02 +0800

    add hash-table solution

--- 1.两数之和.go
+++ 1.两数之和.go
@@ -6,11 +6,30 @@
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
+	// 时间复杂度 O(n2)
+	// for k, v := range nums {
+	// 	vv := target - v
+	// 	for i, j := range nums[k+1:] {
+	// 		if vv == j {
+	// 			return []int{k, i + k + 1}
+	// 		}
+	// 	}
+	// }
+	// return []int{}
+
+	// 时间复杂度 O(n)
+	m := map[int][]int{}
 	for k, v := range nums {
-		vv := target - v
-		for i, j := range nums[k+1:] {
-			if vv == j {
-				return []int{k, i + k + 1}
+		m[v] = append(m[v], k)
+	}
+	for k, v := range m {
+		t := target - k
+		if _, ok := m[t]; ok {
+			if t != k {
+				return []int{v[0], m[t][0]}
+			}
+			if len(m[k]) > 1 {
+				return m[k][0:2]
 			}
 		}
 	}
@@ -18,4 +37,3 @@ func twoSum(nums []int, target int) []int {
 }
 
 // @lc code=end
-
