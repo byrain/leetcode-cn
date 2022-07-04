e250736efee6ffd6beef0d2f2f1c3e81e06df8ac
byrain <byrain@outlook.com>
2022-07-04 17:32:41 +0800

    easy-02

--- 2.两数相加.go
+++ 2.两数相加.go
@@ -13,7 +13,50 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-
+	p := 0
+	var head *ListNode
+	var tail *ListNode
+	for {
+		r := 0
+		if l1 == nil && l2 == nil {
+			if p > 0 {
+				tail.Next = &ListNode{
+					Val:  1,
+					Next: nil,
+				}
+			}
+			return head
+		}
+		if l1 != nil {
+			r = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			r = r + l2.Val
+			l2 = l2.Next
+		}
+		r = r + p
+		if r >= 10 {
+			p = 1
+			r = r - 10
+		} else {
+			p = 0
+		}
+		if head == nil {
+			head = &ListNode{
+				Val:  r,
+				Next: nil,
+			}
+			tail = head
+		} else {
+			tail.Next = &ListNode{
+				Val:  r,
+				Next: nil,
+			}
+			tail = tail.Next
+		}
+	}
 }
+
 // @lc code=end
 
