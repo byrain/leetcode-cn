6b262d0558d34e46f0241ce179b4a3c99bd57a39
byrain <byrain@outlook.com>
2022-07-12 21:14:35 +0800

    dfs

--- 200.岛屿数量.go
+++ 200.岛屿数量.go
@@ -28,32 +28,23 @@ func numIslands(grid [][]byte) int {
 }
 
 func dfs(i, j int, grid [][]byte) {
-	if i >= len(grid) {
+	if i < 0 || i >= len(grid) {
 		return
 	}
-	if i < len(grid) && j >= len(grid[i]) {
+	if j < 0 || (i < len(grid) && j >= len(grid[i])) {
 		return
 	}
 	if grid[i][j] == '0' {
 		return
 	}
+	if used[i][j] {
+		return
+	}
 	used[i][j] = true
-	// up
-	if i-1 >= 0 && grid[i-1][j] == '1' && !used[i-1][j] {
-		dfs(i-1, j, grid)
-	}
-	// down
-	if i+1 < len(grid) && grid[i+1][j] == '1' && !used[i+1][j] {
-		dfs(i+1, j, grid)
-	}
-	// left
-	if j-1 >= 0 && grid[i][j-1] == '1' && !used[i][j-1] {
-		dfs(i, j-1, grid)
-	}
-	// right
-	if j+1 < len(grid[i]) && grid[i][j+1] == '1' && !used[i][j+1] {
-		dfs(i, j+1, grid)
-	}
+	dfs(i-1, j, grid)
+	dfs(i+1, j, grid)
+	dfs(i, j-1, grid)
+	dfs(i, j+1, grid)
 }
 
 // @lc code=end
